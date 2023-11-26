23ef0ea31e97decb6bd47f9197de83064c86b4df
GOC_LADY <lara.coker02@gmail.com>
2023-11-26 17:26:46 +0100

    Second Commit for 26th November

--- app/src/main/java/com/example/myfirstandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstandroidapp/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.myfirstandroidapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val joinButton: Button = this.findViewById(R.id.joinNowButton)
+
+        joinButton.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
+
+            this.startActivity(intent)
+        }
     }
 }
